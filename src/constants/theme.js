81ff8b0aa68e0f8bb52a212a0b7034a4a8685d87
James Didehvar <james@didehvar.com>
2017-08-27 17:16:51 +0100

    switch to deep purple

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,12 +1,12 @@
 import createMuiTheme from 'material-ui/styles/theme';
 import createPalette from 'material-ui/styles/palette';
-import purple from 'material-ui/colors/purple';
+import deepPurple from 'material-ui/colors/deepPurple';
 import teal from 'material-ui/colors/teal';
 
 export const theme = {
   palette: createPalette({
     primary: teal,
-    accent: purple
+    accent: deepPurple
   })
 };
 
