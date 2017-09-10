74ca25f32200fe28a36db317c1cc0d07c40e8b5b
James Didehvar <james@didehvar.com>
2017-09-10 19:17:03 +0100

    persist auth in local storage

--- src/reducers/createStore.js
+++ src/reducers/createStore.js
@@ -3,12 +3,21 @@ import {
   applyMiddleware,
   compose
 } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import thunk from 'redux-thunk';
 
 import rootReducer from './';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const createStore = () =>
-  reduxCreateStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const createStore = () => {
+  const store = reduxCreateStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk), autoRehydrate())
+  );
+
+  persistStore(store, { whitelist: ['auth'] });
+
+  return store;
+};
 
 export default createStore;
