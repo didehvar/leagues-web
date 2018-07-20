ac7c1d6104d1bd3944f423c0a770db1c8584b919
James Didehvar <james@didehvar.com>
2018-07-20 20:12:03 +0100

    remove storybook

--- src/components/AddSegmentForm/AddSegmentForm.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-
-import AddSegmentForm from './index';
-
-storiesOf('AddSegmentForm', module).add('default', () =>
-  <AddSegmentForm onSubmit={action('form-submit')} />
-);
--- src/components/SearchField/SearchField.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import SearchField from './index';
-
-storiesOf('SearchField', module).add('default', () => <SearchField />);
