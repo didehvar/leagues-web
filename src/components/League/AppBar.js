3c6a8e37b0796d3b0fc83b2546bb8bcb9de01a01
James Didehvar <james@didehvar.com>
2018-07-22 15:40:19 +0100

    fix errors

--- src/components/League/AppBar.js
+++ src/components/League/AppBar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/icons/Menu';
+// import MenuIcon from '@material-ui/icons/Menu';
 
-import { MenuButton, Title } from './AppBar.style';
+import { /* MenuButton, */ Title } from './AppBar.style';
 
 class LeagueAppBar extends React.PureComponent {
   render() {
