31e6f1fb618717c3a16250ab3e226b91f0665b7c
James Didehvar <james@didehvar.com>
2018-07-22 22:24:04 +0100

    add header to feed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 import List from '../League/List.container';
 
+import { Container } from './Feed.style';
+
 class Feed extends React.PureComponent {
   render() {
-    return <List userId={132030} />;
+    return (
+      <Container>
+        <Typography paragraph variant="headline">
+          My leagues
+        </Typography>
+
+        <List userId={132030} />
+      </Container>
+    );
   }
 }
 
