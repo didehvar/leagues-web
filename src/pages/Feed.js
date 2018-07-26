74b4e2170b59b9bed5d94251e8d0acd169d9b5ee
James Didehvar <james@didehvar.com>
2018-07-26 15:36:28 +0100

    fix desktop loading

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Loadable from 'react-loadable';
-import Switch from 'react-router-dom/Switch';
 import Route from 'react-router-dom/Route';
 
 import routes from '../utils/routes';
@@ -11,10 +10,6 @@ const Feed = Loadable({
   loading: Loading,
 });
 
-const FeedPage = () => (
-  <Switch>
-    <Route exact path={routes.feed} component={Feed} />
-  </Switch>
-);
+const FeedPage = () => <Route exact path={routes.feed} component={Feed} />;
 
 export default FeedPage;
