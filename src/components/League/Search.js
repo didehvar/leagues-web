1d7e63300980207ad20aa62502408d7b72b79ddf
James Didehvar <james@didehvar.com>
2018-07-28 22:56:03 +0100

    refactor fab

--- src/components/League/Search.js
+++ src/components/League/Search.js
@@ -1,65 +1,24 @@
 import React from 'react';
-import throttle from 'lodash/throttle';
 import Link from 'react-router-dom/Link';
-import Zoom from '@material-ui/core/Zoom';
-import Button from '@material-ui/core/Button';
 import Add from '@material-ui/icons/Add';
 
 import SearchBox from './SearchBox';
 import List from './List.container';
+import FloatingActionButton from '../UI/FloatingActionButton';
 
-import { AddFab } from './Search.style';
 import routes from '../../utils/routes';
 
 class Search extends React.PureComponent {
-  state = { scrolling: true };
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-    this.startScrollTimeout();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  startScrollTimeout = () =>
-    (this.scrollTimeout = setTimeout(
-      () => this.setState({ scrolling: false }),
-      100,
-    ));
-
-  handleScroll = throttle(() => {
-    if (this.scrollTimeout) {
-      clearTimeout(this.scrollTimeout);
-    }
-
-    this.setState({ scrolling: true });
-    this.startScrollTimeout();
-  }, 100);
-
   render() {
-    const { scrolling } = this.state;
-
     return (
       <React.Fragment>
         <SearchBox />
         <List />
 
-        <AddFab>
-          <Zoom in={!scrolling}>
-            <Button
-              variant="extendedFab"
-              color="primary"
-              aria-label="Create"
-              component={Link}
-              to={routes.leagueCreate.path}
-            >
-              <Add />
-              Create
-            </Button>
-          </Zoom>
-        </AddFab>
+        <FloatingActionButton component={Link} to={routes.leagueCreate.path}>
+          <Add />
+          Create
+        </FloatingActionButton>
       </React.Fragment>
     );
   }
---
+++ src/components/UI/FloatingActionButton.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+import throttle from 'lodash/throttle';
+import Link from 'react-router-dom/Link';
+import Zoom from '@material-ui/core/Zoom';
+import Button from '@material-ui/core/Button';
+
+const Container = styled.div`
+  position: fixed;
+  right: ${props => props.theme.spacing.unit * 2}px;
+  bottom: ${props => props.theme.spacing.unit * 9}px;
+
+  ${props => props.theme.breakpoints.up('md')} {
+    left: auto;
+    bottom: ${props => props.theme.spacing.unit * 2}px;
+    transform: none;
+  }
+`;
+
+class FloatingActionButton extends React.PureComponent {
+  state = { scrolling: true };
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+    this.startScrollTimeout();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  startScrollTimeout = () =>
+    (this.scrollTimeout = setTimeout(
+      () => this.setState({ scrolling: false }),
+      100,
+    ));
+
+  handleScroll = throttle(() => {
+    if (this.scrollTimeout) {
+      clearTimeout(this.scrollTimeout);
+    }
+
+    this.setState({ scrolling: true });
+    this.startScrollTimeout();
+  }, 100);
+
+  render() {
+    const { scrolling } = this.state;
+    const { children, ...props } = this.props;
+
+    return (
+      <Container>
+        <Zoom in={!scrolling}>
+          <Button variant="extendedFab" color="primary" {...props}>
+            {children}
+          </Button>
+        </Zoom>
+      </Container>
+    );
+  }
+}
+
+export default FloatingActionButton;
