cb4d1df9b07f3ccc5abbabb7fbd659a5dba9f506
James Didehvar <james@didehvar.com>
2018-07-29 23:19:39 +0100

    attempt to fix chunk error

--- src/components/UI/Loading.js
+++ src/components/UI/Loading.js
@@ -2,14 +2,24 @@ import React from 'react';
 
 import FullPageLoading from '../UI/FullPageLoading';
 
-const Loading = ({ error, retry, pastDelay }) => {
-  if (error || pastDelay) {
-    return (
-      <FullPageLoading errorMessage={error && error.message} retry={retry} />
-    );
-  }
+class Loading extends React.PureComponent {
+  reload = () => window.location.reload();
 
-  return null;
-};
+  render() {
+    const { error, retry, pastDelay } = this.props;
+    const chunkError = error && error.message.startsWith('Loading chunk');
+
+    if (error || pastDelay) {
+      return (
+        <FullPageLoading
+          errorMessage={error && error.message}
+          retry={chunkError ? this.reload : retry}
+        />
+      );
+    }
+
+    return null;
+  }
+}
 
 export default Loading;
